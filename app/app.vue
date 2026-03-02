fa2e9c23753ceb28212f12274afc489365f46299
Frida W <frida.wikman@hotmail.com>
2026-03-02 18:40:21 +0100

    Started creating files

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
-    <NuxtPage />
-  </div>
+  <body
+    class="w-screen flex flex-col justify-center items-center bg-slate-100 text-slate-800"
+  >
+    <div class="w-screen lg:w-[80vw]">
+      <nav>
+        <h1>BokQuiz</h1>
+      </nav>
+      <NuxtPage />
+    </div>
+  </body>
 </template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,8 @@
 <template>
-  <h1>Hej</h1>
+  <div class="min-h-screen flex items-center justify-center bg-slate-100">
+    <div class="bg-white p-8 rounded-xl shadow-lg">
+      <h1 class="text-2xl font-bold text-blue-600">Bokquiz 🚀</h1>
+    </div>
+  </div>
 </template>
+<style scoped></style>
